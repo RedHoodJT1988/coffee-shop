fb94a03444b485bed5303da8da7b003929687988
RedHoodJT1988 <jonathanedwardreeves1988@gmail.com>
2021-07-21 09:35:04 -0500

    Add inital blog post listing

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,7 @@
 import React from 'react';
-
 import { graphql, useStaticQuery } from 'gatsby';
-
+import BlogList from '../components/BlogList';
 import Layout from '../components/Layout';
-
 import styles from './index.module.css';
 
 export default function IndexPage() {
@@ -22,6 +20,7 @@ export default function IndexPage() {
       <div id={styles.hero}>
         <h1>{data.site.siteMetadata.title}</h1>
       </div>
+      <BlogList />
     </Layout>
   );
 }
